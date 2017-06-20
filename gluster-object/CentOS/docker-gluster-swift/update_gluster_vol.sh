49beafda8fd825390cce86896b7b4b8b351c6d6f
venkata edara <redara@redhat.com>
2017-06-20 16:58:13 +0530

    Add S3 Auth to container
    creates a user when container is launched
    
    Signed-off-by: venkata edara <redara@redhat.com>

--- gluster-object/CentOS/docker-gluster-swift/update_gluster_vol.sh
+++ gluster-object/CentOS/docker-gluster-swift/update_gluster_vol.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # To update gluster volume name in swift-volumes, used by swift-gen-builders.service
-if [ -z "$GLUSTER_VOLUMES" ]; then
-        echo "You need to set GLUSTER_VOLUMES env variable"
+if [[ -z "$GLUSTER_VOLUMES" || -z "$GLUSTER_USER" || -z "$GLUSTER_PASSWORD" ]]; then
+        echo "You need to set GLUSTER_VOLUMES, GLUSTER_USER, GLUSTER_PASSWORD env variable"
         exit 1
 else
         echo "GLUSTER_VOLUMES env variable is set. Update in swift-volumes"
         sed -i.bak '/^GLUSTER_VOLUMES=/s/=.*/='\""$GLUSTER_VOLUMES"\"'/' /etc/sysconfig/swift-volumes
+	sed -i.bak '/^GLUSTER_USER=/s/=.*/='\""$GLUSTER_USER"\"'/' /etc/sysconfig/swift-volumes
+	sed -i.bak '/^GLUSTER_PASSWORD=/s/=.*/='\""$GLUSTER_PASSWORD"\"'/' /etc/sysconfig/swift-volumes
 fi
 
 # Hand off to CMD
