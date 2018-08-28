55f88fedbcd3ca2f9c685c1b248196f8cbb4ed1b
Saravanakumar Arumugam <sarumuga@redhat.com>
2018-08-28 18:59:55 +0530

    Add date and script file name in logging
    Signed-off-by: Saravanakumar Arumugam <sarumuga@redhat.com>

--- CentOS/check_size.sh
+++ CentOS/check_size.sh
@@ -8,8 +8,10 @@ do
         current_usage=$( df --output=pcent  '/var/lib/glusterd' | tail -n1  | awk {'print $1'} )
         max_usage=99%
         if [ "${current_usage%?}" -ge "${max_usage%?}" ]; then
-                echo "running out of space in /var/lib/glusterd - stop glusterd"
+                echo "$(date) - $(basename $0): Running out of space in /var/lib/glusterd"
+                echo "$(date) - $(basename $0): Stopping glusterd"
                 systemctl stop glusterd.service
+                echo "$(date) -$(basename $0): Stopped glusterd"
                 break
         fi
 done
