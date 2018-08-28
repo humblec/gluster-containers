2e7be6849acc8304ff478ce4ee85cd041356d63d
Saravanakumar Arumugam <sarumuga@redhat.com>
2018-08-28 16:13:12 +0530

    A simple service to check whether disk full in /var/lib/glusterd stop glusterd service, if so.
    Signed-off-by: Saravanakumar Arumugam <sarumuga@redhat.com>

---
+++ CentOS/readiness_probe.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Disk full
+while true
+do
+        # sleep early to get this settled first.
+        sleep 120
+        current_usage=$( df --output=pcent  '/var/lib/glusterd' | tail -n1  | awk {'print $1'} )
+        max_usage=99%
+        if [ "${current_usage%?}" -ge "${max_usage%?}" ]; then
+                echo "running out of space in /var/lib/glusterd - stop glusterd"
+                systemctl stop glusterd.service
+                break
+        fi
+done
